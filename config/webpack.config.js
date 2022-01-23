899d795bc21e707633a600a37102adffe062ab1b
René <rene@lerchehomepages.de>
2022-01-23 21:40:15 +0100

    adjustments to realize npm link

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path');
 
+const PACKAGE_NAME = '@lechelt/react-financial-charts';
+
 module.exports = {
     entry: path.resolve(__dirname, '../src/index.ts'),
     output: {
         path: path.resolve(__dirname, "../dist"),
-        filename: "react-financial-charts.js",
+        filename: "index.js",
+        libraryTarget: 'umd',
+        library: PACKAGE_NAME,
+        umdNamedDefine: true,
+        globalObject: 'this',
     },
     resolve: {
         extensions: [".js", ".jsx", ".json", ".ts", ".tsx"]
